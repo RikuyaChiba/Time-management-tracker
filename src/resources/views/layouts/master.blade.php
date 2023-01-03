081d7bb0b6fb2c821fddfee2f946973faf444bfe
RikuyaChiba <landlixer@gmail.com>
2023-01-03 15:53:04 +0900

    Import google fonts

--- src/resources/views/layouts/master.blade.php
+++ src/resources/views/layouts/master.blade.php
@@ -14,6 +14,11 @@
     {{-- Google material icons --}}
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    {{-- Google Fonts API --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&family=Open+Sans:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
 
 </head>
 <body>
