74e99e5c0484a6f429114a06a316795dc34fb026
RikuyaChiba <landlixer@gmail.com>
2023-01-03 17:42:01 +0900

    Create sidebar

---
+++ src/resources/views/sidebar.blade.php
@@ -0,0 +1,25 @@
+@section('navbar')
+  <div class="sidebar">
+    <div class="sidebar__logo">
+      <h3>TM tracker</h3>
+    </div>
+    <ul class="sidebar__items">
+      <li class="sidebar__item">
+        <h4 class="material-symbols-outlined">
+          dashboard
+        </h4>
+        <h5 class="ms-3">
+          DashBoard
+        </h5>
+      </li>
+      <li class="sidebar__item">
+        <h4 class="material-symbols-outlined">
+          leaderboard
+        </h4>
+        <h5 class="ms-3">
+          Statistics
+        </h5>
+      </li>
+    </ul>
+  </div>
+@endsection
