6dfa95f72a52bda664f9620b44edaa11ecdd967f
RikuyaChiba <landlixer@gmail.com>
2023-01-12 00:08:43 +0900

    統計ページを作成

--- src/resources/views/sidebar.blade.php
+++ src/resources/views/sidebar.blade.php
@@ -4,29 +4,35 @@
       <h3>TM tracker</h3>
     </div>
     <ul class="sidebar__items">
-      <li class="sidebar__item">
-        <h4 class="material-symbols-outlined">
-          dashboard
-        </h4>
-        <h5 class="ms-3">
-          DashBoard
-        </h5>
+      <li>
+        <a href="{{ route('dashboard') }}" class="sidebar__item">
+          <h4 class="material-symbols-outlined">
+            dashboard
+          </h4>
+          <h5 class="ms-3">
+            DashBoard
+          </h5>
+        </a>
       </li>
       <li class="sidebar__item">
-        <h4 class="material-symbols-outlined">
-          leaderboard
-        </h4>
-        <h5 class="ms-3">
-          Statistics
-        </h5>
+        <a href="{{ route('statistics') }}" class="sidebar__item">
+          <h4 class="material-symbols-outlined">
+            leaderboard
+          </h4>
+          <h5 class="ms-3">
+            Statistics
+          </h5>
+        </a>
       </li>
       <li class="sidebar__item">
-        <h4 class="material-symbols-outlined">
-          delete
-        </h4>
-        <h5 class="ms-3">
-          Archive
-        </h5>
+        <a href="#" class="sidebar__item">
+          <h4 class="material-symbols-outlined">
+            delete
+          </h4>
+          <h5 class="ms-3">
+            Archive
+          </h5>
+        </a>
       </li>
     </ul>
   </div>
