532f81c01e3d3633b3ee7af975cd48b432b34f47
RikuyaChiba <landlixer@gmail.com>
2023-01-15 22:45:45 +0900

    スクロールバーを追加

--- src/resources/views/components/todo_item.blade.php
+++ src/resources/views/components/todo_item.blade.php
@@ -1,15 +1,14 @@
-<div id={{ "quadrantSection" . $section }} class="col-3" data-section={{ $section }}>
-  <div class="todo__section">
-    <div class="todo__section-header">
-      <span class="todo__title">{{ $title }}</span>
-      <span id={{ "sectionPercent" . $section }} class="todo__percent"></span>
-    </div>
-    <div id={{ "todoProgressBar" . $section }} class="todo__progress-bar"></div>
+<div id={{ "quadrantSection" . $section }} class="todo__section col-3" data-section={{ $section }}>
+  <div class="todo__section-header">
+    <span class="todo__title">{{ $title }}</span>
+    <span id={{ "sectionPercent" . $section }} class="todo__percent"></span>
   </div>
-  <div class="todo__item">
+  <div id={{ "todoProgressBar" . $section }} class="todo__progress-bar"></div>
+  <div class="todo__container">
     <div class="todo__header">
       <h5>Todo</h5>
     </div>
+    <div class="todo__items"></div>
     <div id={{ "todoAddButton" . $section }} class="todo__add-button">
       <span class="todo__add-icon material-symbols-outlined">add</span>
       <span class="todo__add-text">Add another card</span>
