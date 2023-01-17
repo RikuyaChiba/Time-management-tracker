3067fb81ef5f0eb43b7e0f982a162559eb43b6c0
RikuyaChiba <landlixer@gmail.com>
2023-01-18 01:21:25 +0900

    統計機能を作成

--- src/app/Http/Controllers/StatisticsController.php
+++ src/app/Http/Controllers/StatisticsController.php
@@ -15,34 +15,76 @@ class StatisticsController extends Controller
     public function index()
     {
       $date = new DateTimeImmutable();
-      $monthly = $this->getThisMonthlyData($date);
 
       $reference = $this->database->getReference('tasks/');
       $snapshot = $reference->getSnapShot();
       $value = $snapshot->getValue();
       // Remove null value from array because firebase insert empty value automatically.
-      // source: https://stackoverflow.com/questions/15534917/why-do-firebase-collections-seem-to-begin-with-a-null-row
+      // https://stackoverflow.com/questions/15534917/why-do-firebase-collections-seem-to-begin-with-a-null-row
       $filter_value = array_filter($value);
+      foreach($filter_value as &$val) {
+        $val['created_at'] = new \DateTime($val['created_at']);
+      }
 
-      // 全部のデータを取得
-      // 1週間のデータが1要素の配列を取得
-      // dataの中に、日付と全体に対する%のデータを入れる
-      //
-      return view('statistics');
+      // それぞれの週のデータを取得
+      $weekly_info = $this->getMonthlyData($date);
+
+      $data = [];
+      // 1週目
+      foreach($weekly_info as $week_num => $week) {
+        // Firebaseで取得したデータから、該当の週のデータだけをフィルタリング
+        $weekly_data = array_filter($filter_value, function($value) use ($week) {
+          return ($value['created_at'] >= $week['start_day'] && $value['created_at'] <= $week['end_day']);
+        });
+        // 週の始まりと終わり
+        $data[$week_num]['period'] = [
+          'start_day' => $week['start_day'],
+          'end_day'   => $week['end_day']
+        ];
+        if (empty($weekly_data)) {
+          continue;
+        }
+        // 週のセクションの割合
+        for($i = 1; $i <= 4; $i++) {
+          $section_data = array_filter($weekly_data, function($value) use ($i) {
+              return $value['section_id'] === $i;
+          });
+          $section_ratio = (count($section_data) / count($weekly_data)) * 100; // 全データに対するセクションの割合
+          $data[$week_num]['section'][$i] = $section_ratio;
+        }
+      }
+      return view('statistics')->with($data);
     }
 
-    private function getThisMonthlyData($date)
+    /**
+     * ひと月の週ごとの始まりと終わりの日付を取得
+     */
+    private function getMonthlyData($date): array
     {
       $year = $date->format('Y');
       $month = $date->format('m');
-      $day = $date->format('d');
-      $monthly['first_monday'] = $date->modify('first Monday of this month');
-      // If first_monday is not 1 of that month, get last sunday of before month.
-      if ($monthly['first_monday'] !== 1) {
-        $monthly['first_monday'] = $date->modify('last Monday of previous month');
-      }
-      $monthly['last_sunday'] = $date->modify('last Sunday of this month');
-      return $monthly;
-    }
+      $last_day = $date->modify('last day of this month')->format('d');
+      // 1週目
+      $data['first_week'] = [
+        'start_day' => new \DateTime($year . '/' . $month . '/' . '01'),
+        'end_day' => new \DateTime($year . '/' . $month . '/' . '07')
+      ];
+      // 2週目
+      $data['second_week'] = [
+        'start_day' => new \DateTime($year . '/' . $month . '/' . '08'),
+        'end_day' => new \DateTime($year . '/' . $month . '/' . 14)
+      ];
+      // 3週目
+      $data['third_week'] = [
+        'start_day' => new \DateTime($year . '/' . $month . '/' . 15),
+        'end_day' => new \DateTime($year . '/' . $month . '/' . 21)
+      ];
+      // 4週目
+      $data['fourth_week'] = [
+        'start_day' => new \DateTime($year . '/' . $month . '/' . 22),
+        'end_day' => new \DateTime($year . '/' . $month . '/' . $last_day)
+      ];
 
+      return $data;
+    }
 }
